8eaa7fa46411697101d336792dc792b5e97e33be
Pixels <pixels67@proton.me>
2025-09-22 04:50:17 +0300

    Remove build dir (oopsie daisy)

--- build/CMakeFiles/engine.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "/Volumes/Data/Projects/engine/out/lib/Darwin-x86_64-/libengine.a"
-)
--- build/CMakeFiles/engine_example.dir/src/examples/example.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/engine_example.dir/src/examples/example.cpp.o: \
-  /Volumes/Data/Projects/engine/src/examples/example.cpp \
-  /Volumes/Data/Projects/engine/include/engine.h
