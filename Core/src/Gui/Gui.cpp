039dab693eaae938bc16c0e852d47a5da4df8114
Pixels <pixels67@proton.me>
2025-10-26 22:10:48 +0300

    Add ImGui

---
+++ Core/src/Gui/Gui.cpp
@@ -0,0 +1,34 @@
+#include "ImGui.hpp"
+
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+#include "Graphics/Gl/Window.hpp"
+
+namespace Pixf::Core::Gui {
+    void Init(const Graphics::Gl::Window &window) {
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGui::StyleColorsDark();
+        ImGui_ImplGlfw_InitForOpenGL(window.GetGlfwWindowPtr(), true);
+        ImGui_ImplOpenGL3_Init("#version 330");
+    }
+
+    void Terminate() {
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+        ImGui::DestroyContext();
+    }
+
+    void BeginRenderGui() {
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+    }
+
+    void EndRenderGui() {
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    }
+} // namespace Pixf::Core::Gui
---
+++ Core/src/Gui/Gui.hpp
@@ -0,0 +1,14 @@
+#ifndef GUIMANAGER_HPP
+#define GUIMANAGER_HPP
+
+#include "Graphics/Gl/Window.hpp"
+
+namespace Pixf::Core::Gui {
+    void Init(const Graphics::Gl::Window &window);
+    void Terminate();
+
+    void BeginRenderGui();
+    void EndRenderGui();
+} // namespace Pixf::Core::Gui
+
+#endif // GUIMANAGER_HPP
